401114aba8acd43be80f5f0528bcc92210f093aa
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-05-29 18:40:23 -0300

    Version inicial (rota)

---
+++ Clicker/Assets/Game.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+	public GameObject click01;
+	public GameObject click02;
+	public GameObject click03;
+
+	public TMP_Text t;
+
+	public GameObject click04;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    t.text = "Gold:" + coso.sc;
+
+	    if (coso.sc > 10)
+	    {
+		    click01.SetActive(true);
+	    }
+	    else
+	    {
+		    click01.SetActive(false);
+	    }
+		if (coso.sc > 50)
+	    {
+		    click02.SetActive(true);
+		}
+	    else
+	    {
+			click02.SetActive(false);
+		}
+		if (coso.sc > 100)
+		{
+			click03.SetActive(true);
+		}
+		else
+		{
+			click03.SetActive(false);
+		}
+	    if (coso.sc > 200)
+	    {
+		    click04.SetActive(true);
+	    }
+	    else
+	    {
+		    click04.SetActive(false);
+		}
+	}
+
+	public void Click01()
+	{
+		coso.sc += 1;
+	}
+
+	public void Click02()
+	{
+		coso.sc += 2;
+	}
+
+	public void Click03()
+	{
+		coso.sc += 5;
+	}
+
+	public void Click04()
+	{
+		coso.sc += 10;
+	}
+
+	public void Click05()
+	{
+		coso.sc += 50;
+	}
+}
---
+++ Clicker/Assets/restart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public void Restart()
+	{
+		coso.sc = 0;
+		SceneManager.LoadScene(0);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
